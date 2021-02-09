95ce41d8cf2233087f56fd0398621926ebc6821a
Davide Pugliese <deviad@gmail.com>
2021-02-10 01:11:03 +0200

    Adding frontend

---
+++ client/src/test/java/io/deviad/ripeti/client/ClientApplicationTests.java
@@ -0,0 +1,11 @@
+package io.deviad.ripeti.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
