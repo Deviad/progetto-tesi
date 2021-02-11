3d9f3dcff366447b1a04d201482e5aa82495b60e
Davide Pugliese <deviad@gmail.com>
2021-02-11 13:36:03 +0200

    Add custom error messages

--- client/src/main/react-app/src/App.tsx
+++ client/src/main/react-app/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import {withTheme} from '@rjsf/core';
+import {FormProps, withTheme} from '@rjsf/core';
 // @ts-ignore
 import {Theme as AntDTheme} from '@rjsf/antd';
 
 import './App.css'
-import {Col, Layout, PageHeader, Row} from "antd";
+import {Col, Layout, PageHeader, Row, Typography} from "antd";
+import Title from "antd/es/typography/Title";
 
 // Make modifications to the theme with your own fields and widgets
 
@@ -12,48 +13,46 @@ const Form = withTheme(AntDTheme);
 
 
 const schema = {
-    "title": "A registration form",
-    "description": "A simple form example.",
     "type": "object",
     "required": [
-        "firstName",
-        "lastName"
+        "prenume",
+        "nume"
     ],
     "properties": {
-        "firstName": {
+        "prenume": {
             "type": "string",
-            "title": "First name",
-            "default": "Chuck"
+            "title": "Prenume",
+            "minLength": 3,
+            "pattern": "[a-zA-Z]+"
         },
-        "lastName": {
+        "nume": {
             "type": "string",
-            "title": "Last name"
+            "title": "Nume",
+            "minLength": 3,
+            "pattern": "[a-zA-Z]+"
         },
         "telephone": {
             "type": "string",
-            "title": "Telephone",
-            "minLength": 10
+            "title": "Telefon",
+            "minLength": 10,
         }
     }
 };
 
 const uiSchema = {
-    "firstName": {
+    "prenume": {
         "ui:autofocus": true,
         "ui:emptyValue": "",
-        "ui:autocomplete": "family-name"
+        "ui:autocomplete": "prenume"
     },
-    "lastName": {
+    "nume": {
         "ui:emptyValue": "",
-        "ui:autocomplete": "given-name"
+        "ui:autocomplete": "nume"
     },
     "age": {
         "ui:widget": "updown",
         "ui:title": "Age of person",
         "ui:description": "(earthian year)"
-    },
-    "bio": {
-        "ui:widget": "textarea"
     },
     "password": {
         "ui:widget": "password",
@@ -69,27 +68,91 @@ const uiSchema = {
     }
 };
 
-const log = (type: any) => console.log.bind(console, type);
-const onSubmit = (data: any) => log(data);
+
+interface EroareDeBaza {
+    message: string,
+    property: string,
+}
+
+interface EroareLipseste extends EroareDeBaza {
+    name: "required"
+}
+
+interface EroareDeLimita extends EroareDeBaza {
+    name: "minLength",
+    params: {
+        limit: number
+    }
+}
+
+interface EroareDePattern extends EroareDeBaza {
+    name: "pattern",
+    params: {
+        pattern: string;
+    }
+}
+
+type EroareGeneric = EroareLipseste | EroareDeLimita | EroareDePattern
+
+function schimbaMesajDeEroare(error: EroareLipseste | EroareDeLimita | EroareDePattern) {
+    if (error.name === "required") {
+
+        error.message = `${error.property} este un camp obligator`
+    }
+
+    else if (error.name === "minLength") {
+        error.message = `${error.property} trebuie sa contina cel putin ${error.params.limit} caractere`
+    }
+
+    else if (error.name === "pattern" && error.params.pattern === "[a-zA-Z]+") {
+        error.message = `${error.property} poate contine doar litere`;
+    }
+}
+
+const transformaEroarile = (errors: [
+    EroareGeneric
+]) => {
+    return errors.map(error => {
+        schimbaMesajDeEroare(error);
+        return error;
+    });
+}
+
+
+// const log = (type: any) => console.log.bind(console, type);
+const onSubmit = (form: FormProps<any>) => {
+    console.log("evt", form);
+};
 
 function App() {
     return (
-
-         <Layout>
-             <Layout.Header>
-             </Layout.Header>
-             <Layout.Content>
-                 <Row gutter={[16, 16]} align="middle">
-                     <Col span={10} flex="auto" push="6">
-                         {/* @ts-ignore */}
-                         <Form schema={schema} uiSchema={uiSchema}
-                               onChange={log("changed")}
-                               onSubmit={onSubmit}
-                               onError={log("errors")}/>
-                     </Col>
-                 </Row>
-             </Layout.Content>
-         </Layout>
+        <Layout>
+            <Layout.Header>
+            </Layout.Header>
+            <PageHeader>
+            </PageHeader>
+            <Layout.Content>
+                <Row gutter={[16, 16]} align="middle" justify="center">
+                    <Col span={10} flex="auto">
+                        <Typography>
+                            <Title>
+                                Inregistreazate
+                            </Title>
+                        </Typography>
+                    </Col>
+                </Row>
+                <Row gutter={[16, 16]} align="middle" justify="center">
+                    <Col span={10} flex="auto">
+                        {/* @ts-ignore */}
+                        <Form schema={schema} uiSchema={uiSchema}
+                              onSubmit={onSubmit}
+                              showErrorList={false}
+                              transformErrors={transformaEroarile}
+                        />
+                    </Col>
+                </Row>
+            </Layout.Content>
+        </Layout>
     );
 }
 
