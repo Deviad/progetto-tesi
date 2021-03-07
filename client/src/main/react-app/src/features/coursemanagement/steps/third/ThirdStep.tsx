331b3320e9e0dff39c5b24cb1f9ed5f4b88321b5
Davide Pugliese <deviad@gmail.com>
2021-03-08 00:22:29 +0200

    Refactoring step 3

--- client/src/main/react-app/src/features/coursemanagement/steps/third/ThirdStep.tsx
+++ client/src/main/react-app/src/features/coursemanagement/steps/third/ThirdStep.tsx
@@ -1,9 +1,8 @@
 import {Button} from "antd";
 import React, {FC} from "react";
-import {v4 as uuidv4} from 'uuid';
 import {WizardStepsState} from "../../WizardSteps";
-import {produce} from 'immer';
 import {QuizList} from "./quiz";
+import {newDefaultQuizAdded} from "./thirdStepCallbacks";
 
 export const ThirdStep: FC<{ state: WizardStepsState, setState: Function }> = ({state, setState}) => {
 
@@ -17,41 +16,7 @@ export const ThirdStep: FC<{ state: WizardStepsState, setState: Function }> = ({
                 <div style={{overflowY: "scroll", height: "40vh"}}>
                     <br/>
                     <br/>
-                    <Button type="primary" onClick={() => {
-                        const quizId = uuidv4();
-                        const questionId = uuidv4();
-                        const answerId = uuidv4();
-
-                        setState(produce((draft: WizardStepsState) => {
-                            draft
-                                .steps[2]
-                                .quizzes[quizId] = {
-                                id: quizId,
-                                questions: {
-                                    [questionId]: {
-                                        modified: false,
-                                        deleted: false,
-                                        id: questionId,
-                                        title: "Completeaza",
-                                        answers: {
-                                            [answerId]: {
-                                                value: true,
-                                                id: answerId,
-                                                title: "Completeaza",
-                                                modified: false,
-                                                deleted: false,
-                                            }
-                                        }
-                                    }
-                                },
-                                quizName: "Introduci o denumire",
-                                quizContent: "Introduci o descriere",
-                                type: "new",
-                                modified: false,
-                                deleted: false,
-                            }
-                        }));
-                    }}>Adauga chestionar</Button>
+                    <Button type="primary" onClick={newDefaultQuizAdded(setState)}>Adauga chestionar</Button>
                     <br/>
                     <br/>
                     <QuizList state={state} setState={setState} keys={keys}/>
---
+++ client/src/main/react-app/src/features/coursemanagement/steps/third/thirdStepCallbacks.tsx
@@ -0,0 +1,41 @@
+import {v4 as uuidv4} from "uuid";
+import {produce} from "immer";
+import {WizardStepsState} from "../../WizardSteps";
+
+export function newDefaultQuizAdded(setState: Function) {
+    return () => {
+        const quizId = uuidv4();
+        const questionId = uuidv4();
+        const answerId = uuidv4();
+
+        setState(produce((draft: WizardStepsState) => {
+            draft
+                .steps[2]
+                .quizzes[quizId] = {
+                id: quizId,
+                questions: {
+                    [questionId]: {
+                        modified: false,
+                        deleted: false,
+                        id: questionId,
+                        title: "Completeaza",
+                        answers: {
+                            [answerId]: {
+                                value: true,
+                                id: answerId,
+                                title: "Completeaza",
+                                modified: false,
+                                deleted: false,
+                            }
+                        }
+                    }
+                },
+                quizName: "Introduci o denumire",
+                quizContent: "Introduci o descriere",
+                type: "new",
+                modified: false,
+                deleted: false,
+            }
+        }));
+    };
+}
