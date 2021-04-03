29a72d8af1d64d944155ad4eedbd77449f8dcc74
Davide Pugliese <deviad@gmail.com>
2021-04-04 02:28:05 +0300

    Mark answer

--- client/src/main/react-app/src/features/dashboard/student/CourseDetail.tsx
+++ client/src/main/react-app/src/features/dashboard/student/CourseDetail.tsx
@@ -1,6 +1,6 @@
 import {useHistory, useParams} from "react-router-dom";
-import React, {useEffect} from "react";
-import {Card, Col, Collapse, Row, Typography} from "antd";
+import React, {MouseEventHandler, useEffect} from "react";
+import {Button, Card, Col, Collapse, Radio, Row, Typography} from "antd";
 import Title from "antd/es/typography/Title";
 import {useState} from "reinspect";
 import {httpGet} from "../../../httpClient";
@@ -8,6 +8,7 @@ import {BASE_URL, COURSE_ENDPOINT} from "../../../constants";
 import {useSelector} from "react-redux";
 import {RootState} from "../../../app/rootReducer";
 import "./courseDetails.scss"
+import {RadioChangeEvent} from "antd/es";
 
 interface ICourseInfo {
     courseName: string;
@@ -42,13 +43,93 @@ interface ICourseInfo {
     }[];
 }
 
+interface QuizState {
+    quizzes: Record<string, {
+        id: string;
+        quizName: string;
+        questions: Record<string, {
+            id: string;
+            title: string;
+            answers: Record<string, {
+                id: string;
+                title: string;
+                value: boolean;
+            }>;
+        }>
+    }>
+}
+
 const {Panel} = Collapse;
+
+export const handleAnswerClick = ({
+                                      quizId,
+                                      questionId,
+                                      answerId,
+                                      setAnswerState,
+                                      answerState
+                                  }: {
+    quizId: string,
+    questionId: string,
+    answerId: string,
+    setAnswerState: Function,
+    answerState: Record<string, any>
+}) =>
+    (evt: RadioChangeEvent) => {
+        const newState = {
+            ...answerState,
+            quizzes: {
+                ...answerState?.quizzes,
+                [quizId]: {
+                    ...answerState[quizId],
+                    questions: {
+                        ...answerState?.quizzes[quizId]?.questions,
+                        [questionId]: {
+                            ...answerState?.quizzes[quizId]?.questions[questionId],
+                            answers: {
+                                ...answerState?.quizzes[quizId]?.questions[questionId]?.answers,
+                                [answerId]: {
+                                    ...answerState?.quizzes[quizId]?.questions[questionId]?.answers[answerId],
+                                    value: evt.target.value
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        setAnswerState(newState);
+    };
+
+const verifyQuiz = ({backendData, state, setState, quizId}: { backendData: QuizState, state: QuizState, setState: Function, quizId: string }) =>
+    (evt: React.MouseEvent<HTMLElement>) => {
+
+        const incorrect = [];
+
+            for (const qs of Object.values(backendData.quizzes[quizId].questions)) {
+                const {id: questionId} = qs;
+                for (const ans of Object.values(qs.answers)) {
+                    const {id: ansId} = ans;
+                    if (backendData?.quizzes[quizId]?.questions[questionId]?.answers[ansId].value !==
+                        state?.quizzes[quizId]?.questions[questionId]?.answers[ansId].value
+                    ) {
+                        incorrect.push(ansId);
+                    }
+                }
+            }
+
+        setState({
+            ...state,
+            errors: incorrect.length
+        });
+    }
 
 export const CourseDetail = () => {
 
     const {id} = useParams<{ id: string }>();
 
     const [state, setState] = useState({} as ICourseInfo, 'course-details');
+
+    const [answerState, setAnswerState] = useState({quizzes: {} as Record<string, any>, errors: 0}, 'set-answer');
 
     const user = useSelector((state: RootState) => state.user);
     const history = useHistory();
@@ -160,6 +241,65 @@ export const CourseDetail = () => {
                 </Row>}
                 <br/>
                 <br/>
+                {state.quizzes && Object.values(state.quizzes).length > 0 && <Row justify="start">
+                    <Col className="ant-col-xs-24--ripeti-card-row ant-col-md-22--ripeti-card-row"
+                         lg={{span: 22, flex: "auto"}} md={{span: 22, flex: "auto", push: 2}}
+                         xs={{flex: "auto", span: 24}}>
+                        <Card className="course-lessons" title="Teste de autoevaluare">
+                            <Collapse accordion>
+                                {Object.values(state.quizzes).map(qi => {
+                                    return (
+                                        <Panel header={qi.quizName} id={qi.id} key={qi.id}
+                                               className="answer-card__body">
+                                            {Object.values(qi.questions).length > 0 &&
+                                            Object.values(qi.questions).map(qs => {
+                                                return (
+                                                    <Card className="quiz-questions" title={qs.title}>
+                                                        {
+                                                            Object.values(qs.answers).length > 0 &&
+                                                            Object.values(qs.answers).map(a => {
+                                                                return (
+                                                                    <div className="answer-container">
+                                                                        <div
+                                                                            style={{marginRight: '1rem'}}>{a.title}</div>
+                                                                        <div>
+                                                                            <Radio.Group onChange={handleAnswerClick({
+                                                                                quizId: qi.id,
+                                                                                questionId: qs.id,
+                                                                                answerId: a.id,
+                                                                                answerState,
+                                                                                setAnswerState
+                                                                            })}>
+                                                                                <Radio value={true}>corect</Radio>
+                                                                                <Radio value={false}>incorect</Radio>
+                                                                            </Radio.Group>
+                                                                        </div>
+                                                                    </div>
+                                                                )
+                                                            })
+                                                        }
+                                                        <br/>
+                                                        <br/>
+                                                        <div>
+                                                            <Button type="primary" onClick={verifyQuiz({
+                                                                backendData: {quizzes: state.quizzes},
+                                                                quizId: qi.id,
+                                                                state: answerState,
+                                                                setState: setAnswerState
+                                                            })}>
+                                                                Verifica
+                                                            </Button>
+                                                        </div> <br />
+                                                        Ai totalizat {answerState.errors + ""} eroari
+                                                    </Card>
+                                                )
+                                            })}
+                                        </Panel>)
+                                })}
+                            </Collapse>
+                        </Card>
+                    </Col>
+                </Row>}
             </Col>
         </>
     );
